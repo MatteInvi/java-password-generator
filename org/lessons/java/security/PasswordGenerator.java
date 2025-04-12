fa0d4df04017052fbc57251b24ac7963cdd149ed
MatteInvi <matteoinvidia@gmail.com>
2025-04-12 12:00:43 +0200

    code cleaning

--- org/lessons/java/security/PasswordGenerator.java
+++ org/lessons/java/security/PasswordGenerator.java
@@ -3,53 +3,58 @@ import java.util.Scanner;
 
 public class PasswordGenerator {
 
-    public static void main(String[] args) throws Exception {
-       String nome;
-       String cognome;
-       String colorePreferito;
-       int giornoNascita;
-       int meseNascita;
-       int annoNascita;
-      
-      
-       Scanner inputNome = new Scanner(System.in);
-       System.out.println("Inserisci il tuo nome");
-       nome = inputNome.nextLine();
+  public static void main(String[] args) throws Exception {
+    
+    // Varibili
+    String nome;
+    String cognome;
+    String colorePreferito;
+    int giornoNascita;
+    int meseNascita;
+    int annoNascita;
 
-       Scanner inputCognome = new Scanner(System.in);
-       System.out.println("Inserisci il tuo cognome");
-       cognome = inputCognome.nextLine();
+    // Input
+    Scanner inputNome = new Scanner(System.in);
+    System.out.println("Inserisci il tuo nome:");
+    nome = inputNome.nextLine();
 
-       Scanner inputColorePreferito = new Scanner(System.in);
-       System.out.println("Inserisci il tuo colore preferito: ");
-       colorePreferito = inputColorePreferito.nextLine();
+    Scanner inputCognome = new Scanner(System.in);
+    System.out.println("Inserisci il tuo cognome:");
+    cognome = inputCognome.nextLine();
 
-       Scanner inputGiornoNascita = new Scanner(System.in);
-       System.out.println("Inserisci il tuo giorno di nascita (GG):");
-       String giornoNascitaInserito = inputGiornoNascita.nextLine();
-       giornoNascita = Integer.parseInt(giornoNascitaInserito);
+    Scanner inputColorePreferito = new Scanner(System.in);
+    System.out.println("Inserisci il tuo colore preferito:");
+    colorePreferito = inputColorePreferito.nextLine();
 
-       Scanner inputMeseNascita = new Scanner(System.in);
-       System.out.println("Inserisci il tuo mese di nascita (MM):");
-       String meseNascitaInserito = inputMeseNascita.nextLine();
-       meseNascita = Integer.parseInt(meseNascitaInserito);
+    Scanner inputGiornoNascita = new Scanner(System.in);
+    System.out.println("Inserisci il tuo giorno di nascita (GG):");
+    String giornoNascitaInserito = inputGiornoNascita.nextLine();
+    giornoNascita = Integer.parseInt(giornoNascitaInserito);
 
-       Scanner inputAnnoNascita = new Scanner(System.in);
-       System.out.println("Inserisci il tuo anno di nascita (YYYY):");
-       String annoNascitaInserito = inputAnnoNascita.nextLine();
-       annoNascita = Integer.parseInt(annoNascitaInserito);
-     
-       int sommaDataNascita = giornoNascita + meseNascita + annoNascita;
-       
-       System.out.print("La tua password poco sicura è " + nome + "-" + cognome + "-" + colorePreferito + "-" + sommaDataNascita );
+    Scanner inputMeseNascita = new Scanner(System.in);
+    System.out.println("Inserisci il tuo mese di nascita (MM):");
+    String meseNascitaInserito = inputMeseNascita.nextLine();
+    meseNascita = Integer.parseInt(meseNascitaInserito);
 
-       inputMeseNascita.close();
-       inputGiornoNascita.close();
-       inputAnnoNascita.close();
-       inputCognome.close();
-       inputNome.close();
-       inputColorePreferito.close();
+    Scanner inputAnnoNascita = new Scanner(System.in);
+    System.out.println("Inserisci il tuo anno di nascita (YYYY):");
+    String annoNascitaInserito = inputAnnoNascita.nextLine();
+    annoNascita = Integer.parseInt(annoNascitaInserito);
 
-  
+    // Calcolo somma data di nascita
+    int sommaDataNascita = giornoNascita + meseNascita + annoNascita;
+
+    // Output
+    System.out.print(
+        "La tua password poco sicura è " + nome + "-" + cognome + "-" + colorePreferito + "-" + sommaDataNascita);
+
+    // Chiusure input
+    inputMeseNascita.close();
+    inputGiornoNascita.close();
+    inputAnnoNascita.close();
+    inputCognome.close();
+    inputNome.close();
+    inputColorePreferito.close();
+
   }
 }
